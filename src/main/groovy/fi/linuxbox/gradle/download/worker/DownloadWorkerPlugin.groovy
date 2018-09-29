97e3454bb0aaf087b304be9b38b623c7690c1644
Mikko Värri <vmj@linuxbox.fi>
2018-09-30 00:13:55 +0300

    Convert DownloadWorkerPlugin to Groovy

---
+++ src/main/groovy/fi/linuxbox/gradle/download/worker/DownloadWorkerPlugin.groovy
@@ -0,0 +1,10 @@
+package fi.linuxbox.gradle.download.worker
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DownloadWorkerPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+    }
+}
